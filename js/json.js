3704317d2474ca84616665da5ea4a48a0a9ce20d
XuMengt <yujiji_x@163.com>
2019-01-29 22:02:13 +0800

    update

--- js/json.js
+++ js/json.js
@@ -10,7 +10,7 @@ let data = {
 				"img/0508/6.jpg"
 			],
 			"title":"SMAEL Digital Watches Kids Dive 50M Water Resistant Wrist Watch Children S Shock Watch LED Clock Kids Sport Watch for Boys",
-			"details":""
+			"details":"<p>    Description:</p><p><br/></p><p>    Function:</p><p><br/></p><p>    - Type: Children Sport Watches</p><p><br/></p><p>    - Numeric dial, three hands dial made with the signature beautiful color which is associated with perfection.&nbsp;</p><p><br/></p><p>    - Dual Movement with the latest breakthrough Japan Original Digital Movement, analog with LED displaying ,date, repeater, time Display, alarm, chronograph,water resistant,complete calender, LED display, S-Shock resistant, stop watch, back light function</p><p><br/></p><p>    - Water resistant up to 5 ATM, 50M Water Resistant , Can Swim With It,Kid Digital Writ Watch</p><p><br/></p><p>    - Showing precise time</p><p><br/></p><p>    -Sport Watch Children Water</p><p><br/></p><p>    -Digital Watch for Kids</p><p><br/></p><p>    &nbsp;</p><p><br/></p><p>    Specification (approximate)</p><p><br/></p><p>    - Case diameter: S:3.6cm approx L:4.2cm approx</p><p><br/></p><p>    - Band Width: S:1.6cm approx L:1.8cm approx</p><p><br/></p><p>    - Band Material: High quality PU</p><p><br/></p><p>    - Band Adjustable: Yes</p><p><br/></p><p>    - Durable gears and material</p><p><br/></p><p>    - Case Material: Strong Rubber + High Strength+Hardened Glass</p><p><br/></p>"
 		},
 		{
 			"imgs":[
@@ -19,8 +19,8 @@ let data = {
 				"img/1376/3.jpg",
 				"img/1376/4.jpg"
 			],
-			"title":"SMAEL Digital Watches Kids Dive 50M Water Resistant Wrist Watch Children S Shock Watch LED Clock Kids Sport Watch for Boys",
-			"details":""
+			"title":"SMAEL Men's Watches Digital Watch Men Clock Male Military Sports Waterproof Wristwatch",
+			"details":'<p>    Description:<span style="white-space: pre;">	</span></p><p>    &nbsp;</p><p>    Cool look just like you, the new matte black color of this sport watches series adds a selection of watches with a distinctively adult character with medium case size .<span style="white-space:pre">	</span></p><p>    The multifunctional dial make timepiece not only fashion look,but also useful when sport &nbsp;.<span style="white-space:pre">	</span></p><p>    &nbsp;</p><p>    Function:<span style="white-space:pre">	</span></p><p>    - Type: Men Sport Watches/Causal Watches<span style="white-space:pre">	</span></p><p>    - Numeric dial, three hands dial made with the signature beautiful color which is associated with perfection.<span style="white-space:pre">	</span></p><p>    - Dual Movement with the latest breakthrough Japan Original Digital Movement, analog with LED displaying ,date, repeater, time Display, alarm, chronograph, water resistant, complete calender, LED display, Shock resistant, stop watch, back light function<span style="white-space:pre">	</span></p><p>    - Water resistant up to 3 ATM, 30M Water Resistant , Can Swim With It<span style="white-space:pre">	</span></p><p>    - Showing precise time<span style="white-space:pre">	</span></p><p>    &nbsp;</p><p>    Package Include:<span style="white-space:pre">	</span></p><p>    &nbsp;-1 * SMAEL Sports Watch<span style="white-space:pre">	</span></p><p>    -1* Instruction CardTo buyers<span style="white-space: pre;">	</span></p><p><br/></p>'
 		},
 		{
 			"imgs":[
@@ -36,18 +36,33 @@ let data = {
 				"img/1545/w3.jpg",
 				"img/1545/w4.jpg",
 			],
-			"title":"SMAEL Digital Watches Kids Dive 50M Water Resistant Wrist Watch Children S Shock Watch LED Clock Kids Sport Watch for Boys",
-			"details":""
+			"title":"SMAEL Mens Wathes Top Brand Luxury Fashion Digital Watch Men Quartz LED Casual Sport Military Watches Men's relogio masculino",
+			"details":"<p>    Brand: SMAEL</p><p><br/></p><p>    Style: Sport,Military,Casual,Business,Fashion</p><p><br/></p><p>    Display: Dual Display Watch</p><p>    Movement: Original Japan Quartz Movement</p><p>    &nbsp;</p><p>    Main Feature:</p><p>    - 100% New with tag and high quality&nbsp;</p><p>    - 5ATM / 50M Water Resistant (Support Cold Shower and Swim, do not operate watch when underwater)</p><p>    - Outdoor Multi-functional Design</p><p>    - Quality Resin Case</p><p>    - Quality PU Strap</p><p>    &nbsp;</p><p>    Specification (approximate):</p><p>    - Case Diameter: 5.0cm</p><p>    - Case Thickness: 1.8cm</p><p>    - Band Width: 2.4m</p><p>    - Total Length: 25.0cm</p><p>    - Product weight: 72g</p>"
 		},
 		{
 			"imgs":[
-				"img/1376/1.jpg",
-				"img/1376/2.jpg",
-				"img/1376/3.jpg",
-				"img/1376/4.jpg"
+				"img/s-l500/ind.jpg",
+				"img/s-l500/inde.jpg",
+				"img/s-l500/index1.jpg",
+				"img/s-l500/index2.jpg",
+				"img/s-l500/s-l500.jpg",
 			],
-			"title":"SMAEL Digital Watches Kids Dive 50M Water Resistant Wrist Watch Children S Shock Watch LED Clock Kids Sport Watch for Boys",
-			"details":""
+			"title":"New SMAEL Digital Bluetooth Men Watch Sport 50M Waterproof Smart Watch",
+			"details":"<p>    Features:</p><p>    - Smartphone connected with App via Bluetooth 4.0. Stable, fast and super low consumption</p><p>    - Pedometer: set your health goal and view your daily activities, check and analyze data anytime</p><p>    - Reminder: Phone call, Message, Twitter Facebook, WhatsApp, Skype</p><p>    - Simple and clear function menu, one button operation, easy to use</p><p>    - LCD displays steps, distance and calories</p><p>    - 3 groups of buzzer sound alarm, you won&#39;t miss anything</p><p>    - Professional stopwatch supports subsection time and background waking</p><p>    - Life waterproof , Do not touch hot water and do not press keys underwater.</p><p>    - 12 months standby time under saving mode, no need to consider replacing battery</p><p>    &nbsp;</p><p>    Specifications:</p><p>    Bluetooth Version: Bluetooth 4.0</p><p>    Support ios Android sports electronic watch</p><p>    Bluetooth calling: Phone call reminder</p><p>    Message: Message reminder</p><p>    Health tracker: Pedometer</p><p>    Remote control: Remote Camera</p><p>    Notification type: Facebook, Skype, Twitter and WhatsApp</p><p>    Groups of Alarm: 3</p><p>    Alert types: Ring</p><p>    Stopwatch: Supported</p><p>    Screen: LED</p><p>    Dial Diameter : 48mm / 1.88 inch</p><p>    Case Thickness : 17mm / 0.66 inch</p><p>    Band Length: 220mm / 8.66 inch</p><p>    Band Width: 21mm / 0.8 inch</p><p>    Case material: Plastic</p><p>    Band material: PU</p><p>    Compatible OS: Android, IOS</p><p>    Compatibility: Android 4.3 / iOS 7.0 and above systems</p><p>    Note: we suggest you not wear it when swim .but &nbsp;if &nbsp;you go to swim or dive , pls don&#39;t &nbsp;press any button underwater!</p><p>    &nbsp;</p><p>    Components Included:</p><p>    -1 x Genuine SMAEL Watch</p><p>    -1 x Use Manual</p><p>    -1 x Simple PVC Bag</p><p><br/></p>"
 		},
+	],
+	"about":[
+		{
+			"title":"Generalize",
+			"content":"Smael's flagship brand is a professional watch manufacturer integrating design, production and sales."
+		},
+		{
+			"title":"management",
+			"content":"Since its establishment, the company has implemented various tasks in strict accordance with the provisions of IS09001/2000, and has established an efficient management team in accordance with the spirit of ISO to scientifically regulate and manage each system link."
+		},
+		{
+			"title":"manufacture",
+			"content":"All products of the company adopt environmentally-friendly materials, and have undergone rigorous testing from raw materials to production and shipping processes, and meet third-party certifications (ROHS/FDA/LFGB/UL... etc.) of customers and various export requirements to ensure that customers are provided.The most guaranteed and assured quality products and services!"
+		}
 	]
 }
